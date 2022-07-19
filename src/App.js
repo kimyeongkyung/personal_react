898841f1faabbf5654c078bc524bbe9062bde2dc
kimyeongkyung <klk17625@naver.com>
2022-07-19 20:43:00 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>hello</div>
+  <>
+    <Head>pratice</Head>
+    <WrapCon>
+      content
+    </WrapCon>
+    </>  
   );
 }
-
+const Head = styled.div`
+  width:100vw;
+  height:100px;
+  background-color:#4DB0DB;
+  font-weight:bold;
+  font-size:30px;
+  color:#fff; 
+  margin:auto;
+  text-align:center;
+  line-height:100px;
+`
+const WrapCon = styled.div`
+`
 export default App;
